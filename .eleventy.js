ec9a345082db10b2ac7af5852bffe0a73c478e4c
Black Project Ventures <blackprojectventures@protonmail.com>
2020-06-01 03:09:32 +0530

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  return {
+    dir: {
+      input: "src",
+      output: "dest",
+    },
+    templateFormats: ["njk"],
+  };
+};
